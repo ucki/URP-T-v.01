372bfe68d98abb4930820daf02fd38890408ac93
ucki <ucki@toppoint.de>
2016-05-10 12:42:20 +0200

    Update X-1-Portlists.sh
    Just removing stray udp & tcp mentions

--- X-1-Portlists.sh
+++ X-1-Portlists.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+#!/bin/bash
 
 echo Making List of  TCP with open / filtered
-for word in $(cat ./results/online-IP.txt); do cat ./results/$word/$word-nmap*.xml |grep portid |cut -d ">" -f 1 | cut -d "=" -f 2,3 |cut -d "\"" -f 2,4 |grep tcp|cut -d "\"" -f 2 |sort -u > ./results/$word/$word-TCPPortlist.txt & done
+for word in $(cat ./results/online-IP.txt); do cat ./results/$word/$word-nmap*.xml |grep portid |cut -d ">" -f 1 | cut -d "=" -f 2,3 |cut -d "\"" -f 2,4 |grep tcp|cut -d "\"" -f 2 |sort -u |grep -v tcp > ./results/$word/$word-TCPPortlist.txt & done
 
 echo Making Inputfile List of  TCP with open / filtered 
 for word in $(cat ./results/online-IP.txt); do paste -sd "," -- ./results/$word/$word-TCPPortlist.txt  > ./results/$word/$word-TCPPortlist-inline.txt & done
@@ -11,7 +12,7 @@ for word in $(cat ./results/online-IP.txt); do paste -sd "," -- ./results/$word/
 
 
 echo Making List of  UDP with open / filtered
-for word in $(cat ./results/online-IP.txt); do cat ./results/$word/$word-nmap*.xml |grep portid |cut -d ">" -f 1 | cut -d "=" -f 2,3 |cut -d "\"" -f 2,4 |grep udp|cut -d "\"" -f 2 |sort -u > ./results/$word/$word-UDPPortlist.txt & done
+for word in $(cat ./results/online-IP.txt); do cat ./results/$word/$word-nmap*.xml |grep portid |cut -d ">" -f 1 | cut -d "=" -f 2,3 |cut -d "\"" -f 2,4 |grep udp|cut -d "\"" -f 2 |sort -u |grep -v udp > ./results/$word/$word-UDPPortlist.txt & done
 
 echo Making Inputfile List of  UDP with open / filtered 
 for word in $(cat ./results/online-IP.txt); do paste -sd "," -- ./results/$word/$word-UDPPortlist.txt  > ./results/$word/$word-UDPPortlist-inline.txt & done
