829ff358c25af6744edb525986b3c6aa74a0e64d
ucki <ucki@users.noreply.github.com>
2016-06-24 13:45:56 +0200

    Update 2-3-1-BasicArachni.sh
    Made it even more tame ....

--- 2-3-1-BasicArachni.sh
+++ 2-3-1-BasicArachni.sh
@@ -4,4 +4,4 @@
 
 #Super aggressive nikto
 echo Arachni
-for word in $(cat ./results/HTTP-IP.txt);do arachni http://$word --output-verbose > ./results/$word/$word-Arachni.txt  & done 
+for word in $(cat ./results/HTTP-IP.txt);do arachni http://$word --output-verbose > ./results/$word/$word-Arachni.txt  ; done 
